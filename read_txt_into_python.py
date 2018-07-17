9303805f88a6a40e03a70b774251e32b3dd575bc
Xiaodong <xzy50@psu.edu>
2018-07-17 15:36:04 -0400

    git tips added

--- read_txt_into_python.py
+++ read_txt_into_python.py
@@ -1,3 +1,4 @@
+#Tom said make another line
 #This is my first Python script with Tom's help July 17th 2018!
 ####work only in /data2/FCHLFGMCCXY_L4_ARAkxqHAABCAAA/cdts-wh.genomics.cn/Upload_06132018/cov/read_txt_into_python.py
 test = open("M-3-2.cov.CX_report.txt","r")
@@ -9,3 +10,23 @@ for line in test:
 
 print(str(i))
 #print(test[1:5])
+
+#also learned 
+# 1079  git status
+# 1080  git status -s
+# 1082  git add read_txt_into_python.py
+# 1083  git status -s
+# 1084  git status 
+# 1085  git commit -m "add new file and practice how to use git"
+# 1086  git status
+# 1087  git config --global user.name "Xiaodong"
+# 1088  git config --global user.email "xzy50@psu.edu"
+# 1089  git config --list
+# 1090  git log
+# 1091  git commit --amend --reset-author
+# 1092  git log
+# 1093  git status
+# 1094  git config --list
+# 1096  git config core.editor "nano"
+# 1097  git config --list
+# 1098  nano read_txt_into_python.py 
