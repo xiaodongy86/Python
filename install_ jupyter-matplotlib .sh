7d1c431e3d026bdb147047d7f3ae15b0909d75b8
xiaodong yang <xiaodongy86@gmail.com>
2021-01-20 19:01:24 -0500

    Create install_ jupyter-matplotlib .sh

---
+++ install_ jupyter-matplotlib .sh
@@ -0,0 +1,5 @@
+installing jupyter-matplotlib 
+conda install -c conda-forge ipympl
+conda install nodejs
+jupyter labextension install @jupyter-widgets/jupyterlab-manager
+jupyter labextension install jupter-matplotlib
